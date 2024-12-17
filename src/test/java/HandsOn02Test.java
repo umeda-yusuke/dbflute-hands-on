0aeae008b30be9b6fc13b11ffa577a155637e26a
umeda-yusuke <yusuke.umeda@bizreach.co.jp>
2024-12-17 17:38:52 +0900

    add test

--- src/test/java/HandsOn02Test.java
+++ src/test/java/HandsOn02Test.java
@@ -1,19 +1,48 @@
-import static org.junit.Assert.assertTrue;
+import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.dbflute.optional.OptionalEntity;
 import org.docksidestage.handson.dbflute.exbhv.MemberBhv;
+import org.docksidestage.handson.dbflute.exentity.Member;
 import org.docksidestage.handson.unit.UnitContainerTestCase;
 
 public class HandsOn02Test extends UnitContainerTestCase {
+
     @Resource
     private MemberBhv memberBhv;
 
-    public void test_existsTestData() throws Exception {
+    public void test_会員名称がSで始まる会員を取得する() throws Exception {
         // ## Arrange ##
         // ## Act ##
-
+        List<Member> members = memberBhv.selectList(cb -> {
+            cb.query().setMemberName_LikeSearch("S", op -> op.likePrefix());
+        });
         // ## Assert ##
-        assertTrue(1 > 0);
+        members.forEach(member -> {
+            assertTrue(member.getMemberName().startsWith("S"));
+        });
+    }
+
+    public void test_会員IDが1の会員を取得する() throws Exception {
+        // ## Arrange ##
+        // ## Act ##
+        OptionalEntity<Member> member = memberBhv.selectEntity(cb -> {
+            cb.query().setMemberId_Equal(1);
+        });
+        // ## Assert ##
+        assertEquals(1, (int) member.get().getMemberId());
+    }
+
+    public void test_生年月日がない会員を取得する() throws Exception {
+        // ## Arrange ##
+        // ## Act ##
+        List<Member> members = memberBhv.selectList(cb -> {
+            cb.query().setBirthdate_IsNull();
+        });
+        // ## Assert ##
+        members.forEach(member -> {
+            assertNull(member.getBirthdate());
+        });
     }
 }
