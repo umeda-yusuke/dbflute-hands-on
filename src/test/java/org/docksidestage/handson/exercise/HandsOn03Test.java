f86649f87eaa21d2eb45dba051a29f03e82f89a1
umeda-yusuke <yusuke.umeda@bizreach.co.jp>
2025-01-21 13:21:31 +0900

    add test_会員セキュリティ情報のリマインダ質問で2という文字が含まれている会員を検索する

--- src/test/java/org/docksidestage/handson/exercise/HandsOn03Test.java
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn03Test.java
@@ -50,9 +50,10 @@ public class HandsOn03Test extends UnitContainerTestCase {
             // Oracle, PostgreSQL とかだと nulls first/last っていうSQL文法があります。
             // MySQLはないので、case when で代用する。DBMSによってSQLの文法がちょっと違う。
             //
-            // TODO umeyan 若い順になってない by jflute (2025/01/14)
-            cb.query().addOrderBy_Birthdate_Asc();
+            // TODO done umeyan 若い順になってない by jflute (2025/01/14)
+            cb.query().addOrderBy_Birthdate_Desc().withNullsLast();
             cb.query().addOrderBy_MemberId_Asc();
+            cb.query().setBirthdate_IsNotNull();
         });
         // [1on1でのふぉろー]
         // MEMBER_SECURITYの方:
@@ -81,9 +82,14 @@ public class HandsOn03Test extends UnitContainerTestCase {
         // もし、突っ込んでみたかったら読んでみて。
     }
 
-    // TODO umeyan サブ要件を読んで改めて実装を進めてみてください by jflute (2025/01/07)
+    // TODO done umeyan サブ要件を読んで改めて実装を進めてみてください by jflute (2025/01/07)
     // javadocにハンズオンのページの検索要件をコピーして持ってくると楽です。
-    
+    /**
+     * 会員セキュリティ情報のデータ自体は要らない
+     * (Actでの検索は本番でも実行されることを想定し、テスト都合でパフォーマンス劣化させないこと)
+     * リマインダ質問に2が含まれていることをアサート
+     * アサートするために別途検索処理を入れても誰も文句は言わない
+     */
     public void test_会員セキュリティ情報のリマインダ質問で2という文字が含まれている会員を検索する() {
         // ## Arrange ##
         // ## Act ##
@@ -95,10 +101,15 @@ public class HandsOn03Test extends UnitContainerTestCase {
         // ## Assert ##
         // [1on1ふぉろー] テスト対象のプログラムが戻すデータですべてアサートが満たせるとは限らない。
         // ので、Assertの中でアサートに必要なデータを自分でかき集めてきても良い。
-        assertHasAnyElement(members);
-        members.forEach(member -> {
-            assertTrue(member.getMemberSecurityAsOne().get().getReminderQuestion().contains("2"));
+        List<Member> reminderQuestion2Members = memberBhv.selectList(cb -> {
+            cb.setupSelect_MemberSecurityAsOne();
+            cb.query().queryMemberSecurityAsOne().setReminderQuestion_LikeSearch("2", op -> op.likeContain());
         });
+        assertHasAnyElement(members);
+        assertEquals(reminderQuestion2Members.size(), members.size());
+        for (int i = 0; i < members.size(); i++) {
+            assertEquals(reminderQuestion2Members.get(i).getMemberName(), members.get(i).getMemberName());
+        }
     }
 
     public void test_会員ステータスの表示順カラムで会員を並べて検索する() {
