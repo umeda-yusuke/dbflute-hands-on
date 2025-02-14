eefa810eeb646d8cb388f72be2cf5da9e308dcca
jflute <dbflute@gmail.com>
2025-02-14 15:59:26 +0900

    section3のdone確認、テストデータの話

--- src/test/java/org/docksidestage/handson/exercise/HandsOn03Test.java
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn03Test.java
@@ -51,6 +51,7 @@ public class HandsOn03Test extends UnitContainerTestCase {
 
     public void test_会員ステータスと会員セキュリティ情報も取得して会員を検索する() {
         // ## Arrange ##
+        
         // ## Act ##
         List<Member> members = memberBhv.selectList(cb -> {
             cb.setupSelect_MemberStatus();
@@ -65,11 +66,17 @@ public class HandsOn03Test extends UnitContainerTestCase {
             // MySQLはないので、case when で代用する。DBMSによってSQLの文法がちょっと違う。
             //
             // done umeyan 若い順になってない by jflute (2025/01/14)
+
             cb.query().addOrderBy_Birthdate_Desc().withNullsLast();
             cb.query().addOrderBy_MemberId_Asc();
             // TODO umeyan 要件を変えたとしても、birthdateがnotnullのものだけに絞るなら NullsLast が不要になる by jflute (2025/01/21)
+            // (setBirthdate_IsNotNull()が入れるのであれば、withNullsLast()は意味のないコードになるのでそれなら消す方が良い)
+            // (もちろん、要件通り setBirthdate_IsNotNull() が不要なのでそっちを消すでも良い)
+            
             // TODO umeyan cbの呼び出し順序の慣習として、select句、where句、order by句... by jflute (2025/01/21)
             // というのがオススメなので、このIsNotNullは、order byよりも前に持っていってくれると嬉しいです。
+            // // 実装順序は、データの取得、絞り込み、並び替え | DBFlute
+            // http://dbflute.seasar.org/ja/manual/function/ormapper/conditionbean/effective.html#implorder
             cb.query().setBirthdate_IsNotNull();
         });
         // [1on1でのふぉろー]
@@ -160,6 +167,7 @@ public class HandsOn03Test extends UnitContainerTestCase {
         // ## Arrange ##
         // ## Act ##
     	// TODO umeyan [最後に修正でOK] "会員ステータスの表示順カラム" なので、会員ステータスコードで並べるわけではない by jflute (2025/01/21)
+        // (要件通りのカラムでのソートになっていない)
         List<Member> members = memberBhv.selectList(cb -> {
             cb.query().addOrderBy_MemberStatusCode_Asc();
             cb.query().addOrderBy_MemberId_Desc();
@@ -167,13 +175,17 @@ public class HandsOn03Test extends UnitContainerTestCase {
         // ## Assert ##
         assertHasAnyElement(members);
         for (int i = 0; i < members.size() - 1; i++) {
-        	// TODO done umeyan [いいね] 変数名がわかりやすい、current, next by jflute (2025/01/21)
+        	// done umeyan [いいね] 変数名がわかりやすい、current, next by jflute (2025/01/21)
             String currentStatusCode = members.get(i).getMemberStatusCode();
             String nextStatusCode = members.get(i + 1).getMemberStatusCode();
             // TODO umeyan 厳密には、一種類の会員ステータスしか検索されなかった場合に、アサートしたいことが成り立たない by jflute (2025/01/21)
             // "<=" の "<" の部分で成り立った assertTrue() が一回以上あることを保証したい。
+            // (一種の素通り防止の話)
             assertTrue(currentStatusCode.compareTo(nextStatusCode) <= 0);
         }
+        // [1on1でのふぉろー] UnitTestでのアサートのやり方パターンの話
+        // o ロジックをどれだけ書くか？話
+        // o テストデータに依存するか？話 (テストデータをどう用意するか？も含めて)
     }
 
     /**
@@ -208,7 +220,7 @@ public class HandsOn03Test extends UnitContainerTestCase {
                     "会員ステータス: " + member.getMemberStatus().get().getMemberStatusName(),
                     "商品名: " + purchase.getProduct().get().getProductName()
             );
-            // TODO done umeyan こっちも抽出した member がそのまま使えるかなと by jflute (2025/01/28)
+            // done umeyan こっちも抽出した member がそのまま使えるかなと by jflute (2025/01/28)
             assertNotNull(member.getBirthdate());
         });
     }
@@ -295,7 +307,7 @@ public class HandsOn03Test extends UnitContainerTestCase {
             cb.setupSelect_Product().withProductStatus();
             cb.setupSelect_Product().withProductCategory();
             
-            // TODO done umeyan 改行しないLambdaであれば、expressionスタイルでOKです (lessThanは大丈夫) by jflute (2025/01/28)
+            // done umeyan 改行しないLambdaであれば、expressionスタイルでOKです (lessThanは大丈夫) by jflute (2025/01/28)
             cb.columnQuery(colCB -> colCB.specify().columnPurchaseDatetime())
                     .greaterEqual(colCB -> colCB.specify().specifyMember().columnFormalizedDatetime());
             cb.columnQuery(colCB -> colCB.specify().columnPurchaseDatetime())
@@ -317,7 +329,7 @@ public class HandsOn03Test extends UnitContainerTestCase {
         // ## Assert ##
         assertHasAnyElement(purchases);
         purchases.forEach(purchase -> {
-            // TODO done umeyan 変数の抽出、今後も意識お願いします。なのでここも by jflute (2025/01/28)
+            // done umeyan 変数の抽出、今後も意識お願いします。なのでここも by jflute (2025/01/28)
             LocalDateTime purchaseDatetime = purchase.getPurchaseDatetime();
             LocalDateTime formalizedDatetime = purchase.getMember().get().getFormalizedDatetime();
             assertTrue(purchaseDatetime.isAfter(formalizedDatetime));
