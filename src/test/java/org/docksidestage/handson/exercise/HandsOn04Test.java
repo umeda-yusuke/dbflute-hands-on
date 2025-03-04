b9f027f82cd41a254c6ee441692c0d4cdf39da57
umeda-yusuke <yusuke.umeda@bizreach.co.jp>
2025-03-04 17:30:17 +0900

    fix classificationDeploymentMap and add HandsOn04Test

---
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn04Test.java
@@ -0,0 +1,22 @@
+package org.docksidestage.handson.exercise;
+
+import javax.annotation.Resource;
+
+import org.docksidestage.handson.dbflute.exbhv.MemberBhv;
+import org.docksidestage.handson.unit.UnitContainerTestCase;
+
+public class HandsOn04Test extends UnitContainerTestCase {
+    @Resource
+    private MemberBhv memberBhv;
+
+    /**
+     * 区分値メソッドの JavaDoc コメントを確認する
+     * 会員ステータス名称も取得する(ログに出力)
+     * 会員が仮会員であることをアサート
+     */
+    public void test_一番若い仮会員の会員を検索する() {
+        // ## Arrange ##
+        // ## Act ##
+        // ## Assert ##
+    }
+}
